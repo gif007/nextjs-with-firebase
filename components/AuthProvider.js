7d12b8e805caadf5e7d7753e6bbeb21706dd72fc
bard <carruthers.n.t@gmail.com>
2021-12-19 15:11:25 -0400

    moved firebase config into separate directory

--- components/AuthProvider.js
+++ components/AuthProvider.js
@@ -2,16 +2,9 @@ import React, { useState, useEffect, useContext, createContext } from "react";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { firebaseConfig } from "../lib/firebaseConfig";
 const AuthContext = createContext({});
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAgEUJB9CiZF-XoYSnsDDPMggzMucI4KgY",
-  authDomain: "dnd99-7df46.firebaseapp.com",
-  projectId: "dnd99-7df46",
-  storageBucket: "dnd99-7df46.appspot.com",
-  messagingSenderId: "901675227818",
-  appId: "1:901675227818:web:e70ae239d48451e9fb0ed3",
-};
 initializeApp(firebaseConfig);
 export const db = getFirestore();
 
